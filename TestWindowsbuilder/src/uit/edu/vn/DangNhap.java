9942d8a0a5b76f37049857112602673cbf888a5a
canhlong1430 <73382249+canhlong1430@users.noreply.github.com>
2020-10-25 09:10:33 +0700

    new popmenu

---
+++ TestWindowsbuilder/src/uit/edu/vn/DangNhap.java
@@ -0,0 +1,64 @@
+package uit.edu.vn;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class DangNhap extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DangNhap frame = new DangNhap();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DangNhap() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				MyUI myui= new MyUI();
+				myui.show();
+			}
+		});
+		panel.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("\u0110\u0103ng Nh\u1EADp");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel.add(lblNewLabel);
+	}
+
+}
